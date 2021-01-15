e5301b7026963a26d3d9918cece680229bdd9876
weiyee097 <weiyee097@gmail.com>
2021-01-15 22:07:58 +0800

    blank page for admin

---
+++ src/Containers/Admin/Admin.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Toolbar from '../../Components/navigation/toolbar/toolbar';
+import { NavLink } from 'react-router-dom';
+import Items from '../../Components/Cart/Items/Items';
+import Button from '../../Components/buttons/grab-offer/grab-btn';
+import Footer from '../../Components/navigation/footer/footer';
+
+export default function Admin () {
+
+    
+       return(
+           <div>
+
+           </div>
+       )
+    
+}
+
