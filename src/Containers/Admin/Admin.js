a92c427e279bf70fd983f0cdc5a7c71143da85eb
weiyee097 <weiyee097@gmail.com>
2021-01-17 13:39:13 +0800

    order manipulation

--- src/Containers/Admin/Admin.js
+++ src/Containers/Admin/Admin.js
@@ -11,26 +11,70 @@ import {
     Text,
     Row
 } from 'reactstrap';
+import RecipeOrders from "../../Components/Recipe/Orders/RecipeOrders";
+import { backend } from "../../Utils/Constants";
+import { connect } from "react-redux";
+import { login } from "../../Redux/Action/User";
+import { loadOrder } from "../../Redux/Action/RecipeOrder";
+
+const mapDispatchToProps = {
+    login,
+    loadOrder
+};
+
+const mapStateToProps = (state) => {
+    let object = {
+        userid: '',
+    }
+    if (state.user != null) {
+        object.userid = state.user._id
+    }
+    return object
+}
 
 class Admin extends Component {
+    componentDidMount() {
+        // first fetch going to remove
+        fetch(backend + '/user/login?email=admin&password=admin', {
+            method: 'GET',
+        }
+        ).then((res) => {
+            return res.json()
+        }).then((data) => {
+            this.props.login(data)
+            fetch(backend + '/order/get-all-recipe-order', { method: 'GET' }).then((res) => {
+                return res.json()
+            }).then((data) => {
+                console.log(data)
+                this.props.loadOrder(data)
+            })
+        }).catch((e) => {
+            console.log(e)
+        })
+    }
+
     render() {
         return (
             <div>
                 <Navbar color="dark">
-                    <NavbarBrand href="/admin">
-                        <h4 className="text-white">
+                    <NavbarBrand>
+                        <h3 className="text-white">
                             <strong>Admin Dashboard</strong>
-                        </h4>
+                        </h3>
                     </NavbarBrand>
+
                     <div expand="md">
                         <Button color="primary">Reset Password</Button>
                         <Button color="primary">Logout</Button>
                     </div>
-
                 </Navbar>
+
+                <section className="Orderitems">
+                    <RecipeOrders />
+                </section>
             </div >
         );
     }
 }
 
-export default Admin;
+export default connect(mapStateToProps, mapDispatchToProps)(Admin);
